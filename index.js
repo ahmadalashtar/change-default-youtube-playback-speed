1ead24ddaaa3bc832b05dad4a0b8d145da2ba1cf
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-03-27 07:38:41 +0200

    Add setInterval to detect URL changes.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.getElementsByClassName("video-stream html5-main-video")[0]. playbackRate = 2;
+function changePlaybackSpeed() {
+    document.getElementsByClassName("video-stream html5-main-video")[0]. playbackRate = 2;
+  }
+  
+let currentUrl = "";
+
+// Check for URL changes every 500ms
+setInterval(function() {
+  if (window.location.href !== currentUrl) {
+    // The URL has changed
+    // Update the current URL
+    changePlaybackSpeed();
+    currentUrl = window.location.href;
+  }
+}, 500);
+
