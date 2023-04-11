2942cb11707b04810537310e5ee94dbbf6b47afe
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-04-11 03:07:12 +0300

    Add event listener navigate-finish to window

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ function changePlaybackSpeed() {
     document.getElementsByClassName("video-stream html5-main-video")[0]. playbackRate = 2;
   }
   
-let currentUrl = "";
-
-// Check for URL changes every 500ms
-setInterval(function() {
-  if (window.location.href !== currentUrl) {
-    // The URL has changed
-    // Update the current URL
-    changePlaybackSpeed();
-    currentUrl = window.location.href;
-  }
-}, 500);
-
+window.addEventListener('yt-navigate-finish',(event)=>{
+  changePlaybackSpeed();
+})
--- toDo.md
+++ toDo.md
@@ -2,4 +2,3 @@ Change text in YouTube's Playback Speed option!
 Build UI.
 Hide me button.
 Cookie write and read.
-use window yt-navigate-finish
