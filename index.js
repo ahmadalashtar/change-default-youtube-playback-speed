b5e802d4fb2030673008e98c1809b54e2f02329f
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-06-28 13:46:00 +0300

    figure out how to change speed text

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function changePlaybackSpeed() {
-    document.getElementsByClassName("video-stream html5-main-video")[0]. playbackRate = 2;
-  }
-  
+/*
+
+تغيير نص السرعة:
+نص السرعة لا يتواجد على اليوتيوب إلا بعد الضغط على أيقونة المسننات
+
+*/
+
+const speed = 2;
+
 window.addEventListener('yt-navigate-finish',(event)=>{
-  changePlaybackSpeed();
+  changePlaybackSpeed(speed);
+  
 })
 
-const changeSpeedText = function(newSpeed){
-  document.getElementsByClassName('ytp-menuitem-content')[2].innerText = newSpeed 
+
+const changePlaybackSpeed = function(speed) {
+  document.getElementsByClassName("video-stream html5-main-video")[0]. playbackRate = speed;
 }
+
+
+
+const set = setInterval(function(){
+    console.log('inside')
+    const currentSpeed = document.getElementsByClassName('ytp-menuitem-content')[1].innerText;
+    console.log    (document.getElementsByClassName('ytp-menuitem-content')[1].innerText)
+    console.log(currentSpeed)
+    if (currentSpeed!=speed) {
+      console.log('changed')
+      document.getElementsByClassName('ytp-menuitem-content')[1].innerText = speed;
+      console.log    (document.getElementsByClassName('ytp-menuitem-content')[1].innerText)
+    console.log(currentSpeed)
+    } else {
+      console.log('stopped')
+      console.log    (document.getElementsByClassName('ytp-menuitem-content')[1].innerText)
+    console.log(currentSpeed)
+    clearInterval(set);
+    }
+}, 1);
