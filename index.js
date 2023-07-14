81a89cbbdf5ad1cb9690282714b0dce7ff95a878
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-07-14 20:03:15 +0300

    Find speed UI element and change its value

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const speed = 2;
 
+const speedOptions = ['Normal', '0.25', '0.75', '0.5', '1.25', '1.75', '1.5', '2', '2.5', '3', '3.5', '4']
+
 window.addEventListener('yt-navigate-finish',(event)=>{
   changePlaybackSpeed(speed);
   
@@ -11,12 +13,32 @@ const changePlaybackSpeed = function(speed) {
 }
 
 
+let item; 
+const speedElement = setInterval(function(){
+  let items = document.getElementsByClassName('ytp-menuitem-content');
+  for (let i = 0 ; i < items.length ; i++){
+    let e = items[i].innerText;
+    if (speedOptions.includes(e)){
+      console.log("here")
+      item = document.getElementsByClassName('ytp-menuitem-content')[i];
+      clearInterval(speedElement);
+      break;
+    }
+  }
+},1)
 
-const set = setInterval(function(){
-    const currentSpeed = document.getElementsByClassName('ytp-menuitem-content')[1].innerText;
-    if (currentSpeed!=speed) {
-      document.getElementsByClassName('ytp-menuitem-content')[1].innerText = speed;
+
+
+
+const SpeedUI = setInterval(function(){
+    // const currentSpeed = document.getElementsByClassName('ytp-menuitem-content')[1].innerText;   
+    if (item.innerText!=speed) {
+      console.log(`speed: ${speed}`)
+      console.log(`item.innerText: ${item.innerText}`)
+      item.innerText = speed;
+      console.log(`speed: ${speed}`)
+      console.log(`item.innerText: ${item.innerText}`)
     } else {
-    clearInterval(set);
+    clearInterval(SpeedUI);
     }
 }, 1);
