97c9cddad0c39495bf314173f40322ba02d86433
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-08-22 17:49:56 +0300

    Use local storage

--- index.js
+++ index.js
@@ -1,60 +1,30 @@
-const main = setInterval(() => {
-  // definitions
-  let speed = 2;
-  const speedOptions = [
-    "Normal",
-    "0.25",
-    "0.75",
-    "0.5",
-    "1.25",
-    "1.75",
-    "1.5",
-    "2",
-    "2.5",
-    "3",
-    "3.5",
-    "4",
-  ];
-  const settingsButtonUI = document.getElementsByClassName(
-    "ytp-settings-button"
+function save(speed) {
+  browser.storage.local.set({ speed: speed });
+}
+
+function main() {
+  const speedElement = document.getElementsByClassName(
+    "video-stream html5-main-video"
   )[0];
-  const changePlaybackSpeed = function (speed) {
-    if (speed == "Normal") speed = "1";
-    document.getElementsByClassName(
-      "video-stream html5-main-video"
-    )[0].playbackRate = speed;
-  };
+  const currentSpeed = document.getElementsByClassName(
+    "video-stream html5-main-video"
+  )[0].playbackRate;
+  speedElement.addEventListener("change", (event) => {
+    main();
+  });
 
-  const changePlaybackSpeedUI = function (item) {
-    if (item.innerText != speed) {
-      item.innerText = speed;
-    }
-  };
+  if (currentSpeed != 1) {
+    save(currentSpeed);
 
-  const findPlaybackSpeedUIElement = function () {
-    let items = document.getElementsByClassName("ytp-menuitem-content");
-    for (let i = 0; i < items.length; i++) {
-      let e = items[i].innerText;
-      if (speedOptions.includes(e)) {
-        const item = document.getElementsByClassName("ytp-menuitem-content")[i];
-        return item;
-      }
-    }
-  };
-
-  if (
-    document.getElementsByClassName("video-stream html5-main-video")[0]
-      .playbackRate == speed
-  ) {
-    clearInterval(main);
-  } else {
-    settingsButtonUI.addEventListener("click", function () {
-      const item = findPlaybackSpeedUIElement();
-      changePlaybackSpeedUI(item);
-    });
-
-    window.addEventListener("yt-navigate-finish", (event) => {
-      changePlaybackSpeed(speed);
-    });
   }
-}, 1);
+  const storedSpeed = browser.storage.local.get("speed");
+  storedSpeed.then(function (result) {
+    if (result) {
+      document.getElementsByClassName(
+        "video-stream html5-main-video"
+      )[0].playbackRate = result.speed;
+    }
+  });
+}
+
+const interval = setInterval(main, 1);
