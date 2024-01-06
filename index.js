2eec3d286e3b208f55bfbed4b220c9b00baf6a98
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-01-06 03:43:59 +0300

    Explain functions, add some todos

--- index.js
+++ index.js
@@ -1,86 +1,86 @@
-// const speedOptions = [
-//   "Normal",
-//   "0.25",
-//   "0.75",
-//   "0.5",
-//   "1.25",
-//   "1.75",
-//   "1.5",
-//   "2",
-// ];
-// const findPlaybackSpeedUIElement = function () {
-//   let items = document.getElementsByClassName("ytp-menuitem-content");
-//   for (let i = 0; i < items.length; i++) {
-//     let e = items[i].innerText;
-//     if (speedOptions.includes(e)) {
-//       const item = document.getElementsByClassName("ytp-menuitem-content")[i];
-//       return item;
-//     }
-//   }
-// };
-// const click = function (speed) {
-//   document.querySelector("button.ytp-settings-button:nth-child(4)")
-//   speedUIElement = findPlaybackSpeedUIElement();
-//   speedUIElement.click();
-//   switch (speed) {
-//       case 0.25:
-//           numericRepresentation = 0;
-//           break;
-//       case 0.5:
-//           numericRepresentation = 1;
-//           break;
-//       case 0.75:
-//           numericRepresentation = 2;
-//           break;
-//       case 1:
-//           numericRepresentation = 3;
-//           break;
-//       case 1.25:
-//           numericRepresentation = 4;
-//           break;
-//       case 1.5:
-//           numericRepresentation = 5;
-//           break;
-//       case 1.75:
-//           numericRepresentation = 6;
-//           break;
-//       case 2:
-//           numericRepresentation = 7;
-//           break;
-//   }
-//   document.getElementsByClassName("ytp-panel")[1].children[1].children[numericRepresentation].click()
-//   document.getElementsByClassName("ytp-panel")[1].children[0].click()
-// }
+/*
+                 speed options:   a list contains speed options 1, 1.5..etc
+find Playback Speed UI Element:   if item.includes(speed options) -> return item
+                         click:   clicks speed element & clicks the desired speed (to get the check mark)
+                          save:   stores the speed variable in the synchronized session storage.
+test this, the only event listener you need is "playing"
 
-// function save(speed) {
-//   browser.storage.sync.set({ speed: speed });
-// }
+*/
+const speedOptions = [
+  "0.25",
+  "0.75",
+  "0.5",
+  "Normal",
+  "1.25",
+  "1.75",
+  "1.5",
+  "2",
+];
+const findPlaybackSpeedUIElement = function () {
+  let items = document.getElementsByClassName("ytp-menuitem-content");
+  for (let i = 0; i < items.length; i++) {
+    let e = items[i].innerText;
+    if (speedOptions.includes(e)) {
+      const item = document.getElementsByClassName("ytp-menuitem-content")[i];
+      return item;
+    }
+  }
+};
+const click = function (speed) {
+  document.querySelector("button.ytp-settings-button:nth-child(4)")
+  speedUIElement = findPlaybackSpeedUIElement();
+  speedUIElement.click();
+  switch (speed) {
+      case 0.25:
+          numericRepresentation = 0;
+          break;
+      case 0.5:
+          numericRepresentation = 1;
+          break;
+      case 0.75:
+          numericRepresentation = 2;
+          break;
+      case 1:
+          numericRepresentation = 3;
+          break;
+      case 1.25:
+          numericRepresentation = 4;
+          break;
+      case 1.5:
+          numericRepresentation = 5;
+          break;
+      case 1.75:
+          numericRepresentation = 6;
+          break;
+      case 2:
+          numericRepresentation = 7;
+          break;
+  }
+  document.getElementsByClassName("ytp-panel")[1].children[1].children[numericRepresentation].click()
+  document.getElementsByClassName("ytp-panel")[1].children[0].click()
+}
 
-// function main() {
-//   const currentSpeed = document.getElementsByClassName(
-//     "video-stream html5-main-video")[0].playbackRate;
-//   const storedSpeed = browser.storage.sync.get("speed");
-//   storedSpeed.then(function (result) {
-//     if (result) {
-//       if (result.speed != currentSpeed) {
-//         click(result.speed);
-//       }
-      
-//     }
-//   });
-//   const speedElement = document.getElementsByClassName(
-//     "video-stream html5-main-video")[0];
-  
-//   speedElement.addEventListener("change", (event) => {
-//     main();
-//   });
+function save(speed) {
+  browser.storage.sync.set({ speed: speed });
+}
 
-//   if (currentSpeed != 1) {
-//     save(currentSpeed);
+function main() {
+  const currentSpeed = document.getElementsByClassName(
+    "video-stream html5-main-video"
+  )[0].playbackRate;
 
-//   }
-
-// }
+  const storedSpeed = browser.storage.sync.get("speed");
+  storedSpeed.then(function (result) {
+    if (result) {
+      document.getElementsByClassName(
+        "video-stream html5-main-video"
+      )[0].playbackRate = result.speed;
+      if (result.speed != currentSpeed) {
+        click(result.speed);
+      }
+    }
+  });
+}
 
 // const interval = setInterval(main, 1);
 
@@ -95,22 +95,17 @@
 
 // this only works once when you refresh the page / open a new page
 // the script only runs once when a refresh happens
-// function showTime() {
-//   var currentdate = new Date();
-//   var datetime = "Last Sync: " + currentdate.getDate() + "/"
-//     + (currentdate.getMonth() + 1) + "/"
-//     + currentdate.getFullYear() + " @ "
-//     + currentdate.getHours() + ":"
-//     + currentdate.getMinutes() + ":"
-//     + currentdate.getSeconds();
-//   console.log(datetime)
-// };
+
 
 
 // This function will be called whenever a new video is selected
 function onChange() {
   console.log("change")
+  main();
   // Add your custom logic here
+}
+function onSpeedChange() {
+  console.log("speed changed")
 }
 
 // Add event listeners
@@ -123,4 +118,13 @@ const videoContainer = document.querySelector(".html5-main-video");
 
 if (videoContainer) {
   videoContainer.addEventListener("playing", onChange); // Triggered when a new video starts playing
+  element = videoContainer.playbackRate;
+  Object.observe(videoContainer, function (changes) {
+    changes.forEach(function (change) {
+      if (change.name === 'playbackRate') {
+        // Update the input field when the playbackRate changes
+        console.log("value changed")
+      }
+    });
+  });
 }
